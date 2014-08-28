b12a7f4e0c1d12c1dbf5497db572b48c74794f6d
Per Magnusson <killingfloor00@gmail.com>
2014-08-28 20:18:00 +0200

    ok

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import sys
 
 
 def main():
+	"""
+	Example usages
+	"""
 	url = ''
 	if not len(sys.argv) > 1:
 		print 'Use URL as argument.'
 		sys.exit(0)
 	url = sys.argv[1]
-	crawl(url)
+	crawl(url, output=True)
 
+	with open('data.txt') as textfile:
+		print '\n\n\n' + textfile.read(),
 
 
 if __name__ == '__main__':
--- pycrawler.py
+++ pycrawler.py
@@ -1,20 +1,27 @@
 import re, urllib2
+from timeout import timeout
 import sys
+
 __all__ = ['crawl']
 
 
 urls = []
 urls_visited = []
-nr = 0
+excluded = ['favicon', '.ico', '.css', '.js', '.jpg', '.jpeg', '.png', '.gif', '#', '?']
+nr = 1
+output = False
+filename = 'data.txt'
 
 
-def crawl(base_url, level=1):
-    global urls, urls_visited, nr
+def crawl(base_url, filename=None, **kwargs):
+    global urls, urls_visited, nr, output
+    if kwargs:
+        if kwargs['output'] is True:
+            output = True
+
     if base_url in urls_visited:
         return
     urls_visited.append(base_url)
-    if level > 30:
-        sys.exit(0)
 
     # get html
     html = get_html(base_url)
@@ -28,14 +35,21 @@ def crawl(base_url, level=1):
     urls = get_urls(base_url, html)
 
     # write to file 
-    nr += 1
     write_to_file(base_url, urls, emails, nr)
+
+    # print
+    if output:
+        print '{0}\t{1}\t{2}'.format(nr, len(urls), len(emails))
+    
+    # count
+    nr += 1
 
     # recursion
     for url in urls:
-        crawl(url, level+1)
+        crawl(url)
 
 
+@timeout(1)
 def get_html(base_url):
     data = None
     try:
@@ -46,11 +60,23 @@ def get_html(base_url):
 
 
 def get_urls(base_url, html):
-    urls = re.findall(r'http://www.[\w.]+', html)
+    global excluded
+    urls = re.findall(r'href="[\'"]?([^\'" >]+)', html)
     urls_unique = []
     for url in urls:
-        if not url in urls_unique and url != base_url:
-            urls_unique.append(url)
+        if not url in urls_unique and url != base_url and not any(word in url for word in excluded):
+            if url[:7] == 'http://' or url[:8] == 'https://' or url[:3] == 'www':
+                urls_unique.append(url)
+            elif len(url) > 100:
+                continue
+            elif url[:3] == '../':
+                url = url[3:]
+            elif url[:2] == './':
+                url = url[2:]
+            elif url[0] == '/' or '.':
+                url = url[1:]
+            else:                
+                urls_unique.append('{0}{1}'.format(base_url, url))
     return urls_unique
     
 
@@ -64,11 +90,30 @@ def get_emails(html):
 
 
 def write_to_file(base_url, urls, emails, nr):
-    with open('data.txt', 'a') as textfile:
+    global filename
+    with open(filename, 'a') as textfile:
         output = '{0}) {1}:\n'.format(nr, base_url)
-        output += 'URLs:\n'
+        output += 'URLs: (%d)\n' % len(urls)
         output += ', '.join(urls)
-        output += '\nE-mails:\n'
+        output += '\nE-mails: (%d)\n' % len(emails)
         output += ', '.join(emails) + '\n\n'
         textfile.write(output)
 
+
+def main():
+    global filename
+    if len(sys.argv) > 2:
+        url = sys.argv[1]
+        filename = sys.argv[2]
+    elif len(sys.argv) > 1:
+        url = sys.argv[1]
+    else:
+        print 'Use URL and textfile as argument: python pycrawler.py http://www.domain.com filename.txt'
+        sys.exit(0)
+    print 'Nr.\tURLs\tE-mails'
+    crawl(url, filename, output=True)
+
+
+
+if __name__ == '__main__':
+    main()
