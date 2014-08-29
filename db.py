a44724ff901a380fb5f2f56a81d11d71eb95cd30
Per Magnusson <killingfloor00@gmail.com>
2014-08-29 18:47:55 +0200

    w sqlite

---
+++ db.py
@@ -0,0 +1,31 @@
+import sqlite3 as lite
+
+class Database:
+    
+    def __init__(self):
+        self._conn = None
+    
+
+    def db_conn(self):
+        conn = None
+        try:
+            conn = lite.connect('data.db')
+            cur = conn.cursor()
+            cur.execute('''CREATE TABLE IF NOT EXISTS url(
+                            id INTEGER PRIMARY KEY,
+                            url TEXT,
+                            title TEXT,
+                            keywords TEXT,
+                            date_added DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+            data = cur.fetchone()
+        except lite.Error, e:
+            print 'Error %s:' % e.args[0]
+            sys.exit(1)
+        self._conn = conn
+        return self._conn
+
+        
+    def insert(self, sql, params):
+        cur = self._conn.cursor()
+        cur.execute(sql, params)
+        self._conn.commit()
--- pycrawler.py
+++ pycrawler.py
@@ -1,119 +1,152 @@
-import re, urllib2
 from timeout import timeout
+from db import Database
+import re, urllib2
 import sys
 
 __all__ = ['crawl']
 
 
-urls = []
-urls_visited = []
-excluded = ['favicon', '.ico', '.css', '.js', '.jpg', '.jpeg', '.png', '.gif', '#', '?']
-nr = 1
-output = False
-filename = 'data.txt'
+class Crawler:
+
+    def __init__(self):
+        self._urls = []
+        self._urls_visited = []
+        self._excluded = ['favicon', '.ico', '.css', '.js', '.jpg', 
+                         '.jpeg', '.png', '.gif', '#', '?']
+        self._nr = 1
+        self._output = False
+        self._filename = 'data.txt'
 
 
-def crawl(base_url, filename=None, **kwargs):
-    global urls, urls_visited, nr, output
-    if kwargs:
-        if kwargs['output'] is True:
-            output = True
+    def crawl(self, base_url, filename=None, **kwargs):
+        if kwargs:
+            if kwargs['output'] is True:
+                self._output = True
 
-    if base_url in urls_visited:
-        return
-    urls_visited.append(base_url)
+        if base_url in self._urls_visited:
+            return
+        self._urls_visited.append(base_url)
 
-    # get html
-    html = get_html(base_url)
-    if not html:
-        return
-    
-    # get emails
-    emails = get_emails(html)
+        # get html
+        html = self.get_html(base_url)
+        if not html:
+            return
+        
+        # get page title
+        title = self.get_page_title(html)
 
-    # get urls
-    urls = get_urls(base_url, html)
+        # get meta keywords
+        keywords = self.get_meta_keywords(html)
 
-    # write to file 
-    write_to_file(base_url, urls, emails, nr)
+        # get emails
+        ### emails = self.get_emails(html)
 
-    # print
-    if output:
-        print '{0}\t{1}\t{2}'.format(nr, len(urls), len(emails))
-    
-    # count
-    nr += 1
+        # get urls
+        self._urls = self.get_urls(base_url, html)
 
-    # recursion
-    for url in urls:
-        crawl(url)
+        # write to db
+        self.write_to_db(base_url, title, keywords)
 
+        # print
+        if self._output:
+            emails = []
+            print '{0}\t{1}\t{2}'.format(self._nr, len(self._urls), len(emails))
+        
+        # count
+        self._nr += 1
 
-@timeout(1)
-def get_html(base_url):
-    data = None
-    try:
-        data = urllib2.urlopen(base_url)
-        return data.read()
-    except:
-        return False
+        # recursion
+        for url in self._urls:
+            self.crawl(url)
 
 
-def get_urls(base_url, html):
-    global excluded
-    urls = re.findall(r'href="[\'"]?([^\'" >]+)', html)
-    urls_unique = []
-    for url in urls:
-        if not url in urls_unique and url != base_url and not any(word in url for word in excluded):
-            if url[:7] == 'http://' or url[:8] == 'https://' or url[:3] == 'www':
-                urls_unique.append(url)
-            elif len(url) > 100:
-                continue
-            elif url[:3] == '../':
-                url = url[3:]
-            elif url[:2] == './':
-                url = url[2:]
-            elif url[0] == '/' or '.':
-                url = url[1:]
-            else:                
-                urls_unique.append('{0}{1}'.format(base_url, url))
-    return urls_unique
-    
-
-def get_emails(html):
-    emails = re.findall(r'[\w.]+@[\w.]+', html)
-    emails_unique = []
-    for email in emails:
-        if not email in emails_unique:
-            emails_unique.append(email)
-    return emails_unique
+    @timeout(1)
+    def get_html(self, base_url):
+        data = None
+        try:
+            data = urllib2.urlopen(base_url)
+            return data.read()
+        except:
+            return False
 
 
-def write_to_file(base_url, urls, emails, nr):
-    global filename
-    with open(filename, 'a') as textfile:
-        output = '{0}) {1}:\n'.format(nr, base_url)
-        output += 'URLs: (%d)\n' % len(urls)
-        output += ', '.join(urls)
-        output += '\nE-mails: (%d)\n' % len(emails)
-        output += ', '.join(emails) + '\n\n'
-        textfile.write(output)
+    def get_urls(self, base_url, html):
+        urls = re.findall(r'href="[\'"]?([^\'" >]+)', html)
+        urls_unique = []
+        for url in urls:
+            if not url in urls_unique and url != base_url and not any(word in url for word in self._excluded):
+                if url[:7] == 'http://' or url[:8] == 'https://' or url[:3] == 'www':
+                    urls_unique.append(url)
+                elif len(url) > 100:
+                    continue
+                elif url[:3] == '../':
+                    url = url[3:]
+                elif url[:2] == './':
+                    url = url[2:]
+                elif url[0] == '/' or '.':
+                    url = url[1:]
+                else:                
+                    urls_unique.append('{0}{1}'.format(base_url, url))
+        return urls_unique
+
+        
+    def get_page_title(self, html):
+        match = re.search(r'<title[^>]*>(.*?)</title>', html)
+        try:
+            title = match.group(1)
+            return title.decode('utf-8')
+        except:
+            return False
 
 
-def main():
-    global filename
-    if len(sys.argv) > 2:
-        url = sys.argv[1]
-        filename = sys.argv[2]
-    elif len(sys.argv) > 1:
-        url = sys.argv[1]
-    else:
-        print 'Use URL and textfile as argument: python pycrawler.py http://www.domain.com filename.txt'
-        sys.exit(0)
-    print 'Nr.\tURLs\tE-mails'
-    crawl(url, filename, output=True)
+    def get_meta_keywords(self, html):
+        try:
+            match = re.search('<meta\sname=["\']keywords["\']\scontent=["\'](.*?)["\']\s/>', html)
+            return match.group(1).decode('utf-8')
+        except:
+            return False
+
+
+    def get_emails(self, html):
+        emails = re.findall(r'[\w.]+@[\w.]+', html)
+        emails_unique = []
+        for email in emails:
+            if not email in emails_unique:
+                emails_unique.append(email)
+        return emails_unique
+
+
+    def write_to_db(self, base_url, title, keywords):
+        db = Database()
+        db.db_conn()
+        db.insert("""INSERT INTO url (url, title, keywords)
+                     VALUES (?, ?, ?)""", 
+                     (base_url, title, keywords))
+
+
+    def write_to_file(self, base_url):
+        with open(self._filename, 'a') as textfile:
+            output = '{0}) {1}:\n'.format(self._nr, base_url)
+            output += 'URLs: (%d)\n' % len(self._urls)
+            output += ', '.join(self._urls)
+            output += '\nE-mails: (%d)\n' % len(self._emails)
+            output += ', '.join(self._emails) + '\n\n'
+            textfile.write(output)
+
+
+    def main(self):
+        if len(sys.argv) > 2:
+            url = sys.argv[1]
+            self._filename = sys.argv[2]
+        elif len(sys.argv) > 1:
+            url = sys.argv[1]
+        else:
+            print 'Use URL and textfile as argument: python pycrawler.py http://www.domain.com filename.txt'
+            sys.exit(0)
+        print 'Nr.\tURLs\tE-mails'
+        self.crawl(url, self._filename, output=True)
 
 
 
 if __name__ == '__main__':
-    main()
+    Crawler().main()
